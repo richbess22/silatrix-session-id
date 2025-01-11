7a2aea2fe9bdef97c7627d5e403c68c47a28c63e
Silva Tech <163982940+SilvaTechB@users.noreply.github.com>
2025-01-11 15:58:07 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -27,8 +27,8 @@ function randomMegaId(length = 6, numberLength = 4) {
 async function uploadCredsToMega(credsPath) {
     try {
         const storage = await new Storage({
-  email: '', // // Your Mega A/c Email Here
-  password: '' // Your Mega A/c Password Here
+  email: 'momanyi.2913@gmail.com', // // Your Mega A/c Email Here
+  password: 'Sylivanus@42620143' // Your Mega A/c Password Here
 }).ready
         console.log('Mega storage initialized.');
         if (!fs.existsSync(credsPath)) {
